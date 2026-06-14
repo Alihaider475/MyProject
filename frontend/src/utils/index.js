197df6e576863ced2e40a5e99b37d028a5e8042b
Alihaider475 <mirzasahib319@gmail.com>
2026-06-14 14:46:04 +0500

    Clean repository files and add environment templates

--- frontend/src/utils/index.js
+++
@@ -1,8 +0,0 @@
-// Shared frontend utility helpers
-export const formatCurrency = (amount, currency = 'PKR') => {
-  return new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: currency,
-    minimumFractionDigits: 0,
-  }).format(amount);
-};
